b16ffb199aa359d8e3dd995d064c06b4c96022c0
Thomas Smith <thomasfn@fortfn.co.uk>
2024-03-28 01:21:13 +0000

    Added CSBlueprintAsyncActionBase that can be extended in C# and is represented by a latent node in BP.

---
+++ Source/UnrealSharpBlueprint/K2Node_CSAsyncAction.h
@@ -0,0 +1,23 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "K2Node_BaseAsyncTask.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/UObjectGlobals.h"
+
+#include "K2Node_CSAsyncAction.generated.h"
+
+class FBlueprintActionDatabaseRegistrar;
+class UObject;
+
+UCLASS()
+class UK2Node_CSAsyncAction : public UK2Node_BaseAsyncTask
+{
+	GENERATED_UCLASS_BODY()
+	
+	// UK2Node interface
+	virtual void GetMenuActions(FBlueprintActionDatabaseRegistrar& ActionRegistrar) const override;
+	// End of UK2Node interface
+};
