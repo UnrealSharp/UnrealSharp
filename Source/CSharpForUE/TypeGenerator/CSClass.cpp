e09134af85fe1c2ef4de8ca6fd7e1111898d705f
Thomas Smith <thomasfn@fortfn.co.uk>
2024-03-29 21:26:10 +0000

    Fixed incorrect assumption that property internal offsets would be aligned to the previous property offset + size in the function struct when invoking managed method.

--- Source/CSharpForUE/TypeGenerator/CSClass.cpp
+++ Source/CSharpForUE/TypeGenerator/CSClass.cpp
@@ -7,7 +7,6 @@
 void UCSClass::InvokeManagedMethod(UObject* ObjectToInvokeOn, FFrame& Stack, RESULT_DECL)
 {
 	const UCSFunction* Function = CastChecked<UCSFunction>(Stack.CurrentNativeFunction);
-	TArray<uint8> ArgumentData;
 	FString ExceptionMessage;
 	bool Success;
 
@@ -19,7 +18,7 @@ void UCSClass::InvokeManagedMethod(UObject* ObjectToInvokeOn, FFrame& Stack, RES
 			++Stack.Code;
 		}
 		
-		Success = InvokeManagedEvent(ObjectToInvokeOn, Function, ArgumentData, ExceptionMessage, RESULT_PARAM);
+		Success = InvokeManagedEvent(ObjectToInvokeOn, Function, TArrayView<const uint8>(), ExceptionMessage, RESULT_PARAM);
 		if (!Success)
 		{
 			const FBlueprintExceptionInfo ExceptionInfo(EBlueprintExceptionType::FatalError, FText::FromString(ExceptionMessage));
@@ -28,11 +27,14 @@ void UCSClass::InvokeManagedMethod(UObject* ObjectToInvokeOn, FFrame& Stack, RES
 		return;
 	}
 	
-    void* LocalStruct = FMemory_Alloca(FMath::Max<int32>(1, Function->GetStructureSize()));
+	int LocalStructSize = Function->GetStructureSize();
+    void* LocalStruct = FMemory_Alloca(FMath::Max<int32>(1, LocalStructSize));
     Function->InitializeStruct(LocalStruct);
 	
     FOutParmRec* OutParameters = nullptr;
     FOutParmRec** LastOut = &OutParameters;
+
+	TArrayView<uint8> ArgumentData((uint8*)FMemory_Alloca(FMath::Max<int32>(1, LocalStructSize)), LocalStructSize);
 	
 	for (TFieldIterator<FProperty> ParamIt(Function, EFieldIteratorFlags::ExcludeSuper); ParamIt; ++ParamIt)
     {
@@ -78,8 +80,13 @@ void UCSClass::InvokeManagedMethod(UObject* ObjectToInvokeOn, FFrame& Stack, RES
                 *LastOut = Out;
             }
         }
-		
-        ArgumentData.Append(ValueAddress, FunctionParameter->GetSize());
+
+		// When C# marshals the parameter to it's own format, it will call GetOffset_ForInternal to determine the offset into ArgumentData to read the parameter from
+		// Let's check that the offset matches where we're currently writing into ArgumentData - if it doesn't, C# will be reading from the wrong place and we have ourselves a nasty alignment error!
+		int InternalOffset = FunctionParameter->GetOffset_ForInternal();
+		int InternalSize = FunctionParameter->GetSize();
+		check((InternalOffset + InternalSize) <= ArgumentData.Num());
+		FMemory::Memcpy(ArgumentData.GetData() + InternalOffset, ValueAddress, InternalSize);
     }
 	
 	if (Stack.Code)
@@ -98,24 +105,24 @@ void UCSClass::InvokeManagedMethod(UObject* ObjectToInvokeOn, FFrame& Stack, RES
 		FBlueprintCoreDelegates::ThrowScriptException(ObjectToInvokeOn, Stack, ExceptionInfo);
 	}
 	
-	ProcessOutParameters(OutParameters, ArgumentData.GetData());
+	ProcessOutParameters(OutParameters, ArgumentData);
 	
 	// Free up memory
 	Function->DestroyStruct(LocalStruct);
 }
 
-void UCSClass::ProcessOutParameters(FOutParmRec* OutParameters, uint8* ArgumentData)
+void UCSClass::ProcessOutParameters(FOutParmRec* OutParameters, TArrayView<const uint8> ArgumentData)
 {
 	for (FOutParmRec* OutParameter = OutParameters; OutParameter != nullptr; OutParameter = OutParameter->NextOutParm)
 	{
-		uint8* ValueAddress = ArgumentData + OutParameter->Property->GetOffset_ForUFunction();
+		const uint8* ValueAddress = ArgumentData.GetData() + OutParameter->Property->GetOffset_ForUFunction();
 		OutParameter->Property->CopyCompleteValue(OutParameter->PropAddr, ValueAddress);
 	}
 }
 
-bool UCSClass::InvokeManagedEvent(UObject* ObjectToInvokeOn, const UCSFunction* Function, TArray<uint8>& ArgumentData, FString& ExceptionMessage, RESULT_DECL)
+bool UCSClass::InvokeManagedEvent(UObject* ObjectToInvokeOn, const UCSFunction* Function, TArrayView<const uint8> ArgumentData, FString& ExceptionMessage, RESULT_DECL)
 {
 	const FGCHandle ManagedObjectHandle = FCSManager::Get().FindManagedObject(ObjectToInvokeOn);
-	int ResultCode = FCSManagedCallbacks::ManagedCallbacks.InvokeManagedMethod(ManagedObjectHandle.GetHandle(), Function->GetManagedMethod(), ArgumentData.GetData(), RESULT_PARAM, &ExceptionMessage);
+	int ResultCode = FCSManagedCallbacks::ManagedCallbacks.InvokeManagedMethod(ManagedObjectHandle.GetHandle(), Function->GetManagedMethod(), (void*)ArgumentData.GetData(), RESULT_PARAM, &ExceptionMessage);
 	return ResultCode == 0;
 }
