f159d6790b95e5d633a1ecfe594d2592a93fec74
Oscar Olsson <101010793+OlssonDev@users.noreply.github.com>
2024-04-14 11:40:50 +0200

    Fixed ActorComponent helper methods and added tooltips

--- Managed/UnrealSharp/UnrealSharp.ExtensionSourceGenerators/ActorComponentExtensionGenerator.cs
+++ Managed/UnrealSharp/UnrealSharp.ExtensionSourceGenerators/ActorComponentExtensionGenerator.cs
@@ -14,15 +14,55 @@ public class ActorComponentExtensionGenerator : ExtensionGenerator
     private void GenerateConstructMethod(ref StringBuilder stringBuilder, INamedTypeSymbol classSymbol)
     {
         string fullTypeName = classSymbol.ToDisplayString();
-        stringBuilder.AppendLine($"     public static {fullTypeName} Construct(Actor owner, SubclassOf<{fullTypeName}>? componentClass = null)");
+        
+        stringBuilder.AppendLine();
+        stringBuilder.AppendLine("     /// <summary>");
+        stringBuilder.AppendLine("     /// Constructs a new component of the specified class, and attaches it to the specified actor.");
+        stringBuilder.AppendLine("     /// </summary>");
+        stringBuilder.AppendLine("     /// <param name=\"owner\">The actor to attach the component to.</param>");
+        stringBuilder.AppendLine("     /// <param name=\"bManualAttachment\">If true, the component will not be attached to the actor's root component.</param>");
+        stringBuilder.AppendLine("     /// <param name=\"relativeTransform\">The relative transform of the component to the actor.</param>");
+        stringBuilder.AppendLine("     /// <returns>The constructed component.</returns>");
+        stringBuilder.AppendLine($"     public static {fullTypeName} Construct(Actor owner, bool bManualAttachment, Transform relativeTransform)");
         stringBuilder.AppendLine("     {");
-        stringBuilder.AppendLine($"         return new {fullTypeName}();");
+        stringBuilder.AppendLine($"         return owner.AddComponentByClass<{fullTypeName}>(bManualAttachment, relativeTransform);");
+        stringBuilder.AppendLine("     }");
+        
+        stringBuilder.AppendLine();
+        stringBuilder.AppendLine("     /// <summary>");
+        stringBuilder.AppendLine("     /// Constructs a new component of the specified class, and attaches it to the specified actor.");
+        stringBuilder.AppendLine("     /// </summary>");
+        stringBuilder.AppendLine("     /// <param name=\"owner\">The actor to attach the component to.</param>");
+        stringBuilder.AppendLine("     /// <param name=\"componentClass\">The class of the component to construct.</param>");
+        stringBuilder.AppendLine("     /// <param name=\"bManualAttachment\">If true, the component will not be attached to the actor's root component.</param>");
+        stringBuilder.AppendLine("     /// <param name=\"relativeTransform\">The relative transform of the component to the actor.</param>");
+        stringBuilder.AppendLine("     /// <returns>The constructed component.</returns>");
+        stringBuilder.AppendLine($"     public static {fullTypeName} Construct(Actor owner, SubclassOf<ActorComponent> componentClass, bool bManualAttachment, Transform relativeTransform)");
+        stringBuilder.AppendLine("     {");
+        stringBuilder.AppendLine($"         return ({fullTypeName}) owner.AddComponentByClass(componentClass, bManualAttachment, relativeTransform);");
+        stringBuilder.AppendLine("     }");
+        
+        stringBuilder.AppendLine();
+        stringBuilder.AppendLine("     /// <summary>");
+        stringBuilder.AppendLine("     /// Constructs a new component of the specified class, and attaches it to the specified actor.");
+        stringBuilder.AppendLine("     /// </summary>");
+        stringBuilder.AppendLine("     /// <param name=\"owner\">The actor to attach the component to.</param>");
+        stringBuilder.AppendLine("     /// <returns>The constructed component.</returns>");
+        stringBuilder.AppendLine($"     public static {fullTypeName} Construct(Actor owner)");
+        stringBuilder.AppendLine("     {");
+        stringBuilder.AppendLine($"         return ({fullTypeName}) owner.AddComponentByClass(typeof({fullTypeName}), false, new Transform());");
         stringBuilder.AppendLine("     }");
     }
     
     private void GenerateComponentGetter(ref StringBuilder stringBuilder, INamedTypeSymbol classSymbol)
     {
         string fullTypeName = classSymbol.ToDisplayString();
+        stringBuilder.AppendLine();
+        stringBuilder.AppendLine("     /// <summary>");
+        stringBuilder.AppendLine("     /// Gets the component of the specified class attached to the specified actor.");
+        stringBuilder.AppendLine("     /// </summary>");
+        stringBuilder.AppendLine("     /// <param name=\"owner\">The actor to get the component from.</param>");
+        stringBuilder.AppendLine("     /// <returns>The component if found, otherwise null.</returns>");
         stringBuilder.AppendLine($"     public static new {fullTypeName}? Get(Actor owner)");
         stringBuilder.AppendLine("     {");
         stringBuilder.AppendLine($"        ActorComponent? foundComponent = owner.GetComponentByClass(typeof({fullTypeName}));");
--- Managed/UnrealSharp/UnrealSharp.ExtensionSourceGenerators/ActorExtensionGenerator.cs
+++ Managed/UnrealSharp/UnrealSharp.ExtensionSourceGenerators/ActorExtensionGenerator.cs
@@ -14,6 +14,17 @@ public class ActorExtensionGenerator : ExtensionGenerator
     private void GenerateSpawnMethod(ref StringBuilder stringBuilder, INamedTypeSymbol classSymbol)
     {
         string fullTypeName = classSymbol.ToDisplayString();
+        stringBuilder.AppendLine();
+        stringBuilder.AppendLine("     /// <summary>");
+        stringBuilder.AppendLine("     /// Spawns an actor of the specified class.");
+        stringBuilder.AppendLine("     /// </summary>");
+        stringBuilder.AppendLine("     /// <param name=\"worldContextObject\">The object to spawn the actor in.</param>");
+        stringBuilder.AppendLine("     /// <param name=\"actorClass\">The class of the actor to spawn.</param>");
+        stringBuilder.AppendLine("     /// <param name=\"spawnTransform\">The transform to spawn the actor at.</param>");
+        stringBuilder.AppendLine("     /// <param name=\"spawnMethod\">The method to handle collisions when spawning the actor.</param>");
+        stringBuilder.AppendLine("     /// <param name=\"instigator\">The actor that caused the actor to be spawned.</param>");
+        stringBuilder.AppendLine("     /// <param name=\"owner\">The actor that owns the spawned actor.</param>");
+        stringBuilder.AppendLine("     /// <returns>The spawned actor.</returns>");
         stringBuilder.AppendLine($"     public static {fullTypeName} Spawn(CoreUObject.Object worldContextObject, SubclassOf<{fullTypeName}> actorClass = default, Transform spawnTransform = default, SpawnActorCollisionHandlingMethod spawnMethod = SpawnActorCollisionHandlingMethod.Default, Pawn? instigator = null, Actor? owner = null)");
         stringBuilder.AppendLine("     {");
         stringBuilder.AppendLine($"         return worldContextObject.SpawnActor<{fullTypeName}>(actorClass, spawnTransform, spawnMethod, instigator, owner);");
@@ -23,6 +34,12 @@ public class ActorExtensionGenerator : ExtensionGenerator
     private void GenerateGetActorsOfClassMethod(ref StringBuilder stringBuilder, INamedTypeSymbol classSymbol)
     {
         string fullTypeName = classSymbol.ToDisplayString();
+        stringBuilder.AppendLine();
+        stringBuilder.AppendLine("     /// <summary>");
+        stringBuilder.AppendLine("     /// Gets all actors of the specified class in the world.");
+        stringBuilder.AppendLine("     /// </summary>");
+        stringBuilder.AppendLine("     /// <param name=\"worldContextObject\">The object to get the actors from.</param>");
+        stringBuilder.AppendLine("     /// <param name=\"outActors\">The list to store the actors in.</param>");
         stringBuilder.AppendLine("     public static new void GetAllActorsOfClass(CoreUObject.Object worldContextObject, out IList<Actor> outActors)");
         stringBuilder.AppendLine("     {");
         stringBuilder.AppendLine($"         GameplayStatics.GetAllActorsOfClass(worldContextObject, typeof({fullTypeName}), out outActors);");
