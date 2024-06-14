7db86f9b3b09d1132b12e9502932da0b698dc535
Arzuor <1754874637@qq.com>
2024-06-14 20:31:36 +0800

    Remove useless file

--- Source/UnrealSharpEditor/CSharpCompileCommand.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include "CSharpCompileCommand.h"
-
-
-#define LOCTEXT_NAMESPACE "FUnrealSharpEditorModule"
-
-void FCSharpCompileCommand::RegisterCommands()
-{
-	UI_COMMAND(PluginAction, "Compile c#", "Compile c#", EUserInterfaceActionType::Button, FInputChord());
-}
