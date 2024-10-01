d0c2c6f403af826644d7e5bde8bd637cc4d91c61
Robert <failmelon@gmail.com>
2024-10-01 09:21:43 -0500

    Revert "Merge pull request #215 from FailMelon/fixed-startup-cooking"
    This reverts commit 9a126884d67f0189df0988865bfab6fdd32f6021, reversing
    changes made to d00abe1f06034650a60116e5e5db39b4e0c74807.

--- Source/CSharpForUE/CSharpForUE.cpp
+++ Source/CSharpForUE/CSharpForUE.cpp
@@ -18,9 +18,6 @@ DEFINE_LOG_CATEGORY(LogUnrealSharp);
 
 void FCSharpForUEModule::StartupModule()
 {
-	// Cooking starts up a new instance of unreal causing this to run, we don't want this
-	if (IsRunningCookCommandlet()) return;
-
 	FCSManager::Get().InitializeUnrealSharp();
 }
 
