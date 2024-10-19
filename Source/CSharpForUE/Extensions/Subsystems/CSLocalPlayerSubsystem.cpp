98b59c77a092adf18abc64df7faa874cd19cdb7a
Oscar Olsson <101010793+OlssonDev@users.noreply.github.com>
2024-10-19 13:22:52 +0200

    Removed old folder

--- Source/CSharpForUE/Extensions/Subsystems/CSLocalPlayerSubsystem.cpp
+++
@@ -1,6 +0,0 @@
-#include "CSLocalPlayerSubsystem.h"
-
-bool UCSLocalPlayerSubsystem::K2_ShouldCreateSubsystem_Implementation() const
-{
-	return true;
-}
--- Source/CSharpForUE/Extensions/Subsystems/CSWorldSubsystem.cpp
+++
@@ -1,6 +0,0 @@
-#include "CSWorldSubsystem.h"
-
-bool UCSWorldSubsystem::K2_ShouldCreateSubsystem_Implementation() const
-{
-	return true;
-}
