ae001113b3cd2221bee90862b45c53fd9d913752
Ashaji <ashaji41@gmail.com>
2024-11-19 18:37:14 +0300

    fix using

--- Managed/UnrealSharp/UnrealSharp.SourceGenerators/CodeAnalyzers/UnrealTypeAnalyzer.cs
+++ Managed/UnrealSharp/UnrealSharp.SourceGenerators/CodeAnalyzers/UnrealTypeAnalyzer.cs
@@ -2,7 +2,6 @@
 using System.Collections.Immutable;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
-using Microsoft.CodeAnalysis.Operations;
 
 namespace UnrealSharp.SourceGenerators.CodeAnalyzers;
 
