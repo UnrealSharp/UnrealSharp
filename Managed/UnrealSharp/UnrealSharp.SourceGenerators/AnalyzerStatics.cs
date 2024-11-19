ea74f7be8d3d5dc1fc8e3178ac825cc0103dc371
Ashaji <ashaji41@gmail.com>
2024-11-19 18:26:56 +0300

    init

--- Managed/UnrealSharp/UnrealSharp.SourceGenerators/AnalyzerStatics.cs
+++ Managed/UnrealSharp/UnrealSharp.SourceGenerators/AnalyzerStatics.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Operations;
 
 namespace UnrealSharp.SourceGenerators;
 
@@ -22,6 +23,12 @@ public static class AnalyzerStatics
     public const string UObject = "UObject";
     public const string AActor = "AActor";
     
+    public const string DefaultComponent = "DefaultComponent";
+    public const string New = "new";
+    public const string UActorComponent = "UActorComponent";
+    public const string USceneComponent = "USceneComponent";
+    public const string UUserWidget = "UUserWidget";
+    
     internal static bool HasAttribute(ISymbol symbol, string attributeName)
     {
         foreach (var attribute in symbol.GetAttributes())
@@ -34,12 +41,25 @@ public static class AnalyzerStatics
 
         return false;
     }
+
+    internal static bool TryGetAttribute(ISymbol symbol, string attributeName, out AttributeData? attribute)
+    {
+        attribute = symbol.GetAttributes()
+            .FirstOrDefault(x => x.AttributeClass is not null && x.AttributeClass.Name == attributeName);
+        
+        return attribute is not null;
+    }
     
     internal static bool HasAttribute(MemberDeclarationSyntax memberDecl, string attributeName)
     {
         return memberDecl.AttributeLists
             .SelectMany(attrList => attrList.Attributes)
             .Any(attr => attr.Name.ToString().Contains(attributeName));
+    }
+
+    internal static bool InheritsFrom(IPropertySymbol propertySymbol, string baseTypeName)
+    {
+        return propertySymbol.Type is INamedTypeSymbol namedTypeSymbol && InheritsFrom(namedTypeSymbol, baseTypeName);
     }
 
     internal static bool InheritsFrom(INamedTypeSymbol symbol, string baseTypeName)
@@ -56,5 +76,27 @@ public static class AnalyzerStatics
         }
 
         return false;
+    }
+
+    internal static bool IsDefaultComponent(AttributeData? attributeData)
+    {
+        if (attributeData?.AttributeClass?.Name != UPropertyAttribute) return false;
+
+        var argument = attributeData.NamedArguments.FirstOrDefault(x => x.Key == DefaultComponent);
+        if (string.IsNullOrWhiteSpace(argument.Key)) return false;
+
+        return argument.Value.Value is true;
+    }
+
+    internal static bool IsNewKeywordInstancingOperation(IObjectCreationOperation operation, out Location? location)
+    {
+        location = null;
+        if (operation.Syntax is not ObjectCreationExpressionSyntax objectCreationExpression)
+        {
+            return false;
+        }
+
+        location = objectCreationExpression.NewKeyword.GetLocation();
+        return objectCreationExpression.NewKeyword.ValueText == New;
     }
 }
---
+++ Managed/UnrealSharp/UnrealSharp.SourceGenerators/CodeSuppressors/DefaultComponentSuppressor.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Immutable;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace UnrealSharp.SourceGenerators.CodeSuppressors;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class DefaultComponentSuppressor : DiagnosticSuppressor
+{
+    public override ImmutableArray<SuppressionDescriptor> SupportedSuppressions => ImmutableArray.Create(DefaultComponentNullableRule);
+    
+    private static readonly SuppressionDescriptor DefaultComponentNullableRule = new(
+        "DefaultComponentNullableSuppressorId",
+        "CS8618",
+        "Default component automatically instantiated."
+    );
+    
+    public override void ReportSuppressions(SuppressionAnalysisContext context)
+    {
+        foreach (var diagnostic in context.ReportedDiagnostics)
+        {
+            var location = diagnostic.Location;
+            var syntaxTree = location.SourceTree;
+            if (syntaxTree is null) continue;
+            
+            var root = syntaxTree.GetRoot(context.CancellationToken);
+            var textSpan = location.SourceSpan;
+            var node = root.FindNode(textSpan);
+            
+            if (node is not PropertyDeclarationSyntax propertyNode || propertyNode.AttributeLists.Count == 0) continue;
+
+            var semanticModel = context.GetSemanticModel(syntaxTree);
+
+            if (IsDefaultComponentProperty(semanticModel, propertyNode, context.CancellationToken))
+            {
+                context.ReportSuppression(Suppression.Create(DefaultComponentNullableRule, diagnostic));
+            }
+        }
+    }
+
+    private static bool IsDefaultComponentProperty(
+        SemanticModel semanticModel, 
+        PropertyDeclarationSyntax propertyNode,
+        CancellationToken cancellationToken)
+    {
+        var symbol = semanticModel.GetDeclaredSymbol(propertyNode, cancellationToken);
+        if (symbol is not IPropertySymbol propertySymbol) return false;
+        
+        return AnalyzerStatics.TryGetAttribute(propertySymbol, AnalyzerStatics.UPropertyAttribute, out var propertyAttribute) && 
+               AnalyzerStatics.IsDefaultComponent(propertyAttribute);
+    }
+}
