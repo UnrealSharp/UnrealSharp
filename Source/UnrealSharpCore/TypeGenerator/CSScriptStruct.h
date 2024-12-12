c91a653c9bff51b0fca88b179c828ba267f3fde6
Ashaji <ashaji41@gmail.com>
2024-12-12 17:08:02 +0300

    init

--- Source/UnrealSharpCore/TypeGenerator/CSScriptStruct.h
+++ Source/UnrealSharpCore/TypeGenerator/CSScriptStruct.h
@@ -1,11 +1,6 @@
 #pragma once
 
-#if ENGINE_MINOR_VERSION >= 5
 #include "StructUtils/UserDefinedStruct.h"
-#else
-#include "Engine/UserDefinedStruct.h"
-#endif
-
 #include "CSScriptStruct.generated.h"
 
 UCLASS()
