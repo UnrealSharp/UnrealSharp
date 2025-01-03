746b960ba99481f729ca136006241dffd81ada9b
Oscar Olsson <101010793+OlssonDev@users.noreply.github.com>
2025-01-03 22:25:00 +0100

    Add generic methods to retrieve owning player and local player as specific types

---
+++ Managed/UnrealSharp/UnrealSharp/Extensions/UMG/Widget.cs
@@ -0,0 +1,19 @@
+﻿using UnrealSharp.Engine;
+
+namespace UnrealSharp.UMG;
+
+public partial class UWidget
+{
+    /// <summary>
+    /// Get the owning player of this widget as a specific type.
+    /// </summary>
+    /// <returns>The owning player of this widget.</returns>
+    public T OwningPlayerAs<T>() where T : APlayerController => (T) OwningPlayer;
+    
+    /// <summary>
+    /// Get the owning local player of this widget as a specific type.
+    /// </summary>
+    /// <typeparam name="T">The type of the local player.</typeparam>
+    /// <returns>The owning local player of this widget.</returns>
+    public T OwningLocalPlayerAs<T>() where T : ULocalPlayer => (T) OwningLocalPlayer;
+}
