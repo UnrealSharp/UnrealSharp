38700cc13743de8be5fc6c86d774f5b328f14903
Robert <failmelon@gmail.com>
2025-01-24 14:24:12 -0600

    Exposed some methods from FPackageName

---
+++ Source/UnrealSharpCore/Extensions/Libraries/CSPackageNameExtensions.cpp
@@ -0,0 +1,16 @@
+﻿#include "CSPackageNameExtensions.h"
+
+void UCSPackageNameExtensions::RegisterMountPoint(const FString& RootPath, const FString& ContentPath)
+{
+	FPackageName::RegisterMountPoint(RootPath, ContentPath);
+}
+
+void UCSPackageNameExtensions::UnRegisterMountPoint(const FString& RootPath, const FString& ContentPath)
+{
+	FPackageName::UnRegisterMountPoint(RootPath, ContentPath);
+}
+
+bool UCSPackageNameExtensions::MountPointExists(const FString& RootPath)
+{
+	return FPackageName::MountPointExists(RootPath);
+}
---
+++ Source/UnrealSharpCore/Extensions/Libraries/CSPackageNameExtensions.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "CSPackageNameExtensions.generated.h"
+
+class UMovementComponent;
+
+UCLASS(meta = (Internal))
+class UCSPackageNameExtensions : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+
+	/**
+	 * This will insert a mount point at the head of the search chain (so it can overlap an existing mount point and win).
+	 *
+	 * @param RootPath Logical Root Path.
+	 * @param ContentPath Content Path on disk.
+	 */
+	UFUNCTION(meta = (ExtensionMethod, ScriptMethod))
+	static void RegisterMountPoint(const FString& RootPath, const FString& ContentPath);
+	
+	/**
+	 * This will remove a previously inserted mount point.
+	 *
+	 * @param RootPath Logical Root Path.
+	 * @param ContentPath Content Path on disk.
+	 */
+	UFUNCTION(meta = (ExtensionMethod, ScriptMethod))
+	static void UnRegisterMountPoint(const FString& RootPath, const FString& ContentPath);
+	
+	/**
+	 * Returns whether the specific logical root path is a valid mount point.
+	 */
+	UFUNCTION(meta = (ExtensionMethod, ScriptMethod))
+	static bool MountPointExists(const FString& RootPath);
+};
