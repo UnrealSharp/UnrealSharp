71242dd8c28ff07e9183f559fdcced327262d3b1
Oscar Olsson <101010793+OlssonDev@users.noreply.github.com>
2025-02-01 22:22:49 +0100

    Fixed build errors for 5.4

--- Source/UnrealSharpCore/TypeGenerator/Register/CSGeneratedStructBuilder.cpp
+++ Source/UnrealSharpCore/TypeGenerator/Register/CSGeneratedStructBuilder.cpp
@@ -39,7 +39,9 @@ void FCSGeneratedStructBuilder::PurgeStruct()
 	Field->MinAlignment = 0;
 	Field->RefLink = nullptr;
 #if WITH_EDITOR
+#if ENGINE_MINOR_VERSION >= 5
 	Field->TotalFieldCount = 0;
+#endif
 	Field->PrimaryStruct = nullptr;
 	Field->EditorData = nullptr;
 #endif
