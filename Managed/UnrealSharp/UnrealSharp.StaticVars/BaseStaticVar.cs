1e6d8354820e8e4075d3328d687b6b0a8e8e7b51
Oscar Olsson <101010793+OlssonDev@users.noreply.github.com>
2025-02-02 11:10:00 +0100

    Fixed FBaseStaticVar causing silent crash on PIE exit

--- Managed/UnrealSharp/UnrealSharp.StaticVars/BaseStaticVar.cs
+++ Managed/UnrealSharp/UnrealSharp.StaticVars/BaseStaticVar.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Runtime.Loader;
 
 namespace UnrealSharp.StaticVars;
 
@@ -6,13 +7,26 @@ public class FBaseStaticVar<T>
 {
     [DebuggerBrowsable(DebuggerBrowsableState.Never)]
     private T? _value;
-
+    
     public virtual T? Value
     {
         get => _value;
         set => _value = value;
     }
+    
+#if !PACKAGE
+    public FBaseStaticVar()
+    {
+        AssemblyLoadContext alc = AssemblyLoadContext.GetLoadContext(GetType().Assembly)!;
+        alc.Unloading += OnAlcUnloading;
+    }
 
+    protected virtual void OnAlcUnloading(AssemblyLoadContext alc)
+    {
+        alc.Unloading -= OnAlcUnloading;
+    }
+#endif
+    
     public override string ToString()
     {
         return Value.ToString();
