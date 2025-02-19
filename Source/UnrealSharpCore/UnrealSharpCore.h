3814ce26d567aeec88eaf6411a1d2b255a7c4e84
Oscar Olsson <101010793+OlssonDev@users.noreply.github.com>
2025-02-19 19:09:43 +0100

    Fixed 5.3 compiler issues

--- Source/UnrealSharpCore/UnrealSharpCore.h
+++ Source/UnrealSharpCore/UnrealSharpCore.h
@@ -3,6 +3,12 @@
 #include "CoreMinimal.h"
 #include "Modules/ModuleManager.h"
 
+#if ENGINE_MINOR_VERSION >= 4
+#define CS_EInternalObjectFlags_AllFlags EInternalObjectFlags_AllFlags
+#else
+#define CS_EInternalObjectFlags_AllFlags EInternalObjectFlags::AllFlags
+#endif
+
 DECLARE_LOG_CATEGORY_EXTERN(LogUnrealSharp, Log, All);
 
 class FUnrealSharpCoreModule : public IModuleInterface
