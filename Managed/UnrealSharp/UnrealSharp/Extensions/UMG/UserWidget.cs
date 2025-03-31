c9a5dbe9ef3d4ff56d646167b6439f821987acfc
Oscar Olsson <101010793+OlssonDev@users.noreply.github.com>
2025-03-31 19:38:39 +0200

    Added extensions to easier get game framework types from userwidget

--- Managed/UnrealSharp/UnrealSharp/Extensions/UMG/UserWidget.cs
+++ Managed/UnrealSharp/UnrealSharp/Extensions/UMG/UserWidget.cs
@@ -1,4 +1,5 @@
 ﻿using UnrealSharp.Engine;
+using UnrealSharp.UnrealSharpCore;
 
 namespace UnrealSharp.UMG;
 
@@ -10,4 +11,34 @@ public partial class UUserWidget
     /// <typeparam name="T">The type of the owning player controller.</typeparam>
     /// <returns>The owning player controller of this widget as the specified type.</returns>
     public T OwningPlayerPawnAs<T>() where T : APawn => (T) OwningPlayerPawn;
+
+    /// <summary>
+    /// Get the owning player state of this widget.
+    /// </summary>
+    public APlayerState OwningPlayerState => UCSUserWidgetExtensions.GetOwningPlayerState(this);
+    
+    /// <summary>
+    /// Get the owning player state of this widget as a specific type.
+    /// </summary>
+    public T OwningPlayerStateAs<T>() where T : APlayerState => (T) OwningPlayerState;
+    
+    /// <summary>
+    /// Get the owning local player of this widget
+    /// </summary>
+    public ULocalPlayer OwningLocalPlayer => UCSUserWidgetExtensions.GetOwningLocalPlayer(this);
+    
+    /// <summary>
+    /// Get the owning local player of this widget as a specific type.
+    /// </summary>
+    public T OwningLocalPlayerAs<T>() where T : ULocalPlayer => (T) OwningLocalPlayer;
+    
+    /// <summary>
+    /// Get the owning player controller of this widget.
+    /// </summary>
+    public APlayerController OwningPlayerController => UCSUserWidgetExtensions.GetOwningPlayerController(this);
+    
+    /// <summary>
+    /// Get the owning player controller of this widget as a specific type.
+    /// </summary>
+    public T OwningPlayerControllerAs<T>() where T : APlayerController => (T) OwningPlayerController;
 }
---
+++ Source/UnrealSharpCore/Extensions/Libraries/CSUserWidgetExtensions.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "CSUserWidgetExtensions.generated.h"
+
+UCLASS(meta = (Internal))
+class UCSUserWidgetExtensions : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(meta=(ScriptMethod))
+	static APlayerController* GetOwningPlayerController(UUserWidget* UserWidget);
+
+	UFUNCTION(meta=(ScriptMethod))
+	static void SetOwningPlayerController(UUserWidget* UserWidget, APlayerController* PlayerController);
+
+	UFUNCTION(meta=(ScriptMethod))
+	static APlayerState* GetOwningPlayerState(UUserWidget* UserWidget);
+
+	UFUNCTION(meta=(ScriptMethod))
+	static ULocalPlayer* GetOwningLocalPlayer(UUserWidget* UserWidget);
+};
