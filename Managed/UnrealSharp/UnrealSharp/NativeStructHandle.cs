5bcb955ad1d254a8791899ae8e294a375ba56e36
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2025-07-08 18:05:54 -0400

    Added a new C# class to manage the native struct

---
+++ Managed/UnrealSharp/UnrealSharp/NativeStructHandle.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.InteropServices;
+using UnrealSharp.CoreUObject;
+using UnrealSharp.Interop;
+
+namespace UnrealSharp;
+
+public class NativeStructHandle(IntPtr nativeScriptStruct) : SafeHandle(IntPtr.Zero, true)
+{
+    public IntPtr NativeStructPtr { get; private set; } = UScriptStructExporter.CallAllocateNativeStruct(nativeScriptStruct);
+
+    public override bool IsInvalid => NativeStructPtr == IntPtr.Zero;
+
+    protected override bool ReleaseHandle()
+    {
+        if (NativeStructPtr == IntPtr.Zero) return true;
+        
+        UScriptStructExporter.CallDeallocateNativeStruct(nativeScriptStruct, NativeStructPtr);
+        NativeStructPtr = IntPtr.Zero;
+
+        return true;
+    }
+}
