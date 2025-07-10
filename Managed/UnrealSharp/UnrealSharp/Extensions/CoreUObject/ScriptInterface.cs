41760ecf97b5879a96c8edbb4febd1b3003c36a1
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2025-07-10 17:32:17 -0400

    Added methods to convert between UObjects and interfaces

--- Managed/UnrealSharp/UnrealSharp/Extensions/CoreUObject/ScriptInterface.cs
+++ Managed/UnrealSharp/UnrealSharp/Extensions/CoreUObject/ScriptInterface.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
 using UnrealSharp.Core;
 using UnrealSharp.Interop;
@@ -23,6 +24,97 @@ public interface IScriptInterface
     
 }
 
+public static class ScriptInterfaceExtensions 
+{
+    /// <summary>
+    /// Attempt to cast a UObject to an interface.
+    /// </summary>
+    /// <param name="uobject">The source UObject.</param>
+    /// <typeparam name="T">The interface type</typeparam>
+    /// <returns>The cast interface, or null if the conversion could not be made.</returns>
+    public static T? AsInterface<T>(this UObject? uobject) where T : class
+    {
+        switch (uobject)
+        {
+            case null:
+                return null;
+            case T typedObj:
+                return typedObj;
+        }
+
+        var nativeClass = typeof(T).TryGetNativeInterface();
+        if (nativeClass == IntPtr.Zero)
+        {
+            return null;
+        }
+            
+        var wrapperHandle = FCSManagerExporter.CallFindOrCreateManagedInterfaceWrapper(uobject.NativeObject, nativeClass);
+        GCHandle wrapperGcHandle = GCHandle.FromIntPtr(wrapperHandle);
+        if (!wrapperGcHandle.IsAllocated)
+        {
+            return null;
+        }
+
+        if (wrapperGcHandle.Target is T typedWrapper)
+        {
+            return typedWrapper;
+        }
+			
+        return null;
+    }
+
+    /// <summary>
+    /// Attempt to cast a UObject to an interface.
+    /// </summary>
+    /// <param name="uobject">The source UObject.</param>
+    /// <typeparam name="T">The interface type</typeparam>
+    /// <returns>The cast interface, or null if the object was null.</returns>
+    /// <exception cref="InvalidCastException">If a non-null object could not be converted</exception>
+    [return: NotNullIfNotNull(nameof(uobject))]
+    public static T? CastInterface<T>(this UObject? uobject) where T : class
+    {
+        if (uobject is null)
+        {
+            return null;
+        }
+        
+        return uobject.AsInterface<T>() ?? throw new InvalidCastException($"Cannot cast {uobject.GetType()} to {typeof(T)}");
+    }
+    
+    /// <summary>
+    /// Attempt to convert an interface object to a UObject.
+    /// </summary>
+    /// <param name="scriptInterface">The interface object</param>
+    /// <returns>The cast UObject or null if the conversion could not be made.</returns>
+    public static UObject? AsUObject(this object? scriptInterface)
+    {
+        return scriptInterface switch
+        {
+            UObject uobject => uobject,
+            IScriptInterface scriptInterfaceObject => scriptInterfaceObject.Object,
+            _ => null
+        };
+
+    }
+
+    /// <summary>
+    /// Attempt to convert an interface object to a UObject.
+    /// </summary>
+    /// <param name="scriptInterface">The interface object</param>
+    /// <returns>The cast UObject or null if the argument is null.</returns>
+    /// <exception cref="InvalidCastException">If a non-null object could not be converted</exception>
+    [return: NotNullIfNotNull(nameof(scriptInterface))]
+    public static UObject? CastUObject(this object? scriptInterface)
+    {
+        if (scriptInterface is null)
+        {
+            return null;
+        }
+        
+        return scriptInterface.AsUObject() ?? throw new InvalidCastException($"Cannot cast {scriptInterface.GetType()} to UObject");
+    }
+}
+
 
 
 public static class ScriptInterfaceMarshaller<T> where T : class
@@ -32,17 +124,7 @@ public static class ScriptInterfaceMarshaller<T> where T : class
         unsafe
         {
             FScriptInterface* scriptInterface = (FScriptInterface*)(nativeBuffer + arrayIndex * sizeof(FScriptInterface));
-            IntPtr objectPointer = IntPtr.Zero;
-            
-            if (obj is UObject unrealObject)
-            {
-                objectPointer = unrealObject.NativeObject;
-            }
-            else if (obj is IScriptInterface scriptInterfaceObject)
-            {
-                objectPointer = scriptInterfaceObject.Object.NativeObject;
-            }
-
+            IntPtr objectPointer = obj.AsUObject()?.NativeObject ?? IntPtr.Zero;
             scriptInterface->ObjectPointer = objectPointer;
             scriptInterface->InterfacePointer = objectPointer;
         }
@@ -55,30 +137,7 @@ public static class ScriptInterfaceMarshaller<T> where T : class
             FScriptInterface* scriptInterface = (FScriptInterface*)(nativeBuffer + arrayIndex * sizeof(FScriptInterface));
             IntPtr handle = FCSManagerExporter.CallFindManagedObject(scriptInterface->ObjectPointer);
             var uobject = GCHandleUtilities.GetObjectFromHandlePtr<UObject>(handle);
-            if (uobject == null)
-            {
-                return null;
-            }
-
-            var nativeClass = typeof(T).TryGetNativeInterface();
-            if (nativeClass == IntPtr.Zero)
-            {
-                return null;
-            }
-            
-            var wrapperHandle = FCSManagerExporter.CallFindOrCreateManagedInterfaceWrapper(uobject.NativeObject, nativeClass);
-            GCHandle wrapperGcHandle = GCHandle.FromIntPtr(wrapperHandle);
-            if (!wrapperGcHandle.IsAllocated)
-            {
-                return null;
-            }
-
-            if (wrapperGcHandle.Target is T typedWrapper)
-            {
-                return typedWrapper;
-            }
-			
-			return null;
+            return uobject.AsInterface<T>();
         }
     }
 }
