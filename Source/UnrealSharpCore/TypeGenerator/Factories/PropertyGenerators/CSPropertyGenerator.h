b6a33bdc0e74c275d72713a048afcf07ed91e9a9
Oscar Olsson <101010793+OlssonDev@users.noreply.github.com>
2025-08-09 10:38:32 +0200

    Fixed up header

--- Source/UnrealSharpCore/TypeGenerator/Factories/PropertyGenerators/CSPropertyGenerator.h
+++ Source/UnrealSharpCore/TypeGenerator/Factories/PropertyGenerators/CSPropertyGenerator.h
@@ -10,24 +10,19 @@ UCLASS(Abstract)
 class UNREALSHARPCORE_API UCSPropertyGenerator : public UObject
 {
 	GENERATED_BODY()
+public:
+	
+	virtual FProperty* CreateProperty(UField* Outer, const FCSPropertyMetaData& PropertyMetaData);
+	virtual bool SupportsPropertyType(ECSPropertyType InPropertyType) const;
+	virtual TSharedPtr<FCSUnrealType> CreateTypeMetaData(ECSPropertyType PropertyType);
 
 protected:
 
 	virtual ECSPropertyType GetPropertyType() const;
 	virtual FFieldClass* GetPropertyClass();
 
-	static bool CanBeHashed(const FProperty* InParam);
-
 	FProperty* NewProperty(UField* Outer, const FCSPropertyMetaData& PropertyMetaData, const FFieldClass* FieldClass = nullptr);
 
 	static UClass* TryFindingOwningClass(UField* Outer);
-	
-public:
-
-
-	
-	virtual FProperty* CreateProperty(UField* Outer, const FCSPropertyMetaData& PropertyMetaData);
-	virtual bool SupportsPropertyType(ECSPropertyType InPropertyType) const;
-	virtual TSharedPtr<FCSUnrealType> CreateTypeMetaData(ECSPropertyType PropertyType);
-
+	static bool CanBeHashed(const FProperty* InParam);
 };
