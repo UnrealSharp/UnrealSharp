5d5ebc593e501b23358e01fd2a27762a1153a112
Robert <failmelon@gmail.com>
2025-09-18 17:29:35 -0500

    Added FTransform.Inversed property

--- Managed/UnrealSharp/UnrealSharp/Extensions/CoreUObject/Transform.cs
+++ Managed/UnrealSharp/UnrealSharp/Extensions/CoreUObject/Transform.cs
@@ -24,6 +24,28 @@ public partial struct FTransform
     public FVector Scale;
     private double u1;
 
+    public FTransform Inversed
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get
+        {
+            const double SmallNumber = 1e-8;
+
+            // Invert the scale
+            FVector invScale = GetSafeScaleReciprocal(Scale, SmallNumber);
+
+            // Invert the rotation
+            FQuat invRotation = FQuat.Inverse(Rotation);
+
+            // Invert the translation
+            FVector scaledTranslation = invScale * Location;
+            FVector t2 = invRotation.RotateVector(scaledTranslation);
+            FVector invTranslation = t2 * -1.0;
+
+            return new FTransform(invRotation, invTranslation, invScale);
+        }
+    }
+
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public FVector TransformPosition(FVector v)
     {
@@ -74,6 +96,17 @@ public partial struct FTransform
     {
         FQuat inverseRotation = FQuat.Inverse(Rotation);
         return inverseRotation.RotateVector(v);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static FVector GetSafeScaleReciprocal(FVector V, double tolerance)
+    {
+        return new FVector
+        (
+            Math.Abs(V.X) <= tolerance ? 0.0 : 1.0 / V.X,
+            Math.Abs(V.Y) <= tolerance ? 0.0 : 1.0 / V.Y,
+            Math.Abs(V.Z) <= tolerance ? 0.0 : 1.0 / V.Z
+        );
     }
 
     public static readonly FTransform ZeroTransform = new(FQuat.Identity, FVector.Zero, FVector.Zero);
