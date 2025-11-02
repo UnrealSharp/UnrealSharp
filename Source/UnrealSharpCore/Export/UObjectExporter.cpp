59b82331ae213c52edefef9f34efef1907859609
Robert Acksel <robert.acksel@googlemail.com>
2025-11-02 18:23:37 +0100

    fix #577 -adding fast path evaluation and readding fast path in case we have a native function to invoke.

--- Source/UnrealSharpCore/Export/UObjectExporter.cpp
+++ Source/UnrealSharpCore/Export/UObjectExporter.cpp
@@ -31,33 +31,107 @@ void UUObjectExporter::NativeGetName(UObject* Object, FName* OutName)
 	*OutName = !IsValid(Object) ? NAME_None : Object->GetFName();
 }
 
+void EvaluateInvokePath(UObject* NativeObject, UFunction* NativeFunction, uint8* Params, uint8* ReturnValueAddress)
+{
+	if (NativeFunction->HasAllFunctionFlags(FUNC_HasOutParms))
+	{
+		UUObjectExporter::InvokeNativeFunctionOutParms(NativeObject, NativeFunction, Params, ReturnValueAddress);
+	}
+    //if the function is an event and not native we would go through UObject::ProcessEvent to avoid stack corruption since it will call into BP code
+	else if (!NativeFunction->HasAnyFunctionFlags(FUNC_Native) && NativeFunction->HasAnyFunctionFlags(FUNC_Event))
+	{
+		NativeObject->ProcessEvent(NativeFunction, Params);
+	}
+    //if the function is native we can go through the fast path. it could also contain the event flag which is common for u# functions
+	else
+	{
+		FFrame NewStack(NativeObject, NativeFunction, Params, nullptr, NativeFunction->ChildProperties);
+		NativeFunction->Invoke(NativeObject, NewStack, ReturnValueAddress);
+	}
+}
+
 void UUObjectExporter::InvokeNativeFunction(UObject* NativeObject, UFunction* NativeFunction, uint8* Params, uint8* ReturnValueAddress)
 {
 	TRACE_CPUPROFILER_EVENT_SCOPE(UUObjectExporter::InvokeNativeFunction);
 
-	NativeObject->ProcessEvent(NativeFunction, Params);
+    EvaluateInvokePath(NativeObject, NativeFunction, Params, ReturnValueAddress);
 }
 
 void UUObjectExporter::InvokeNativeStaticFunction(UClass* NativeClass, UFunction* NativeFunction, uint8* Params, uint8* ReturnValueAddress)
 {
 	TRACE_CPUPROFILER_EVENT_SCOPE(UUObjectExporter::InvokeNativeStaticFunction);
-	UObject* ClassDefaultObject = NativeClass->GetDefaultObject();
 
-	ClassDefaultObject->ProcessEvent(NativeFunction, Params);
+	UObject* ClassDefaultObject = NativeClass->GetDefaultObject();
+	EvaluateInvokePath(ClassDefaultObject, NativeFunction, Params, ReturnValueAddress);
 }
 
 void UUObjectExporter::InvokeNativeNetFunction(UObject* NativeObject, UFunction* NativeFunction, uint8* Params, uint8* ReturnValueAddress)
 {
 	TRACE_CPUPROFILER_EVENT_SCOPE(UUObjectExporter::InvokeNativeNetFunction);
-	
-	NativeObject->ProcessEvent(NativeFunction, Params);
+
+	int32 FunctionCallspace = NativeObject->GetFunctionCallspace(NativeFunction, nullptr);
+
+	if (FunctionCallspace & FunctionCallspace::Remote)
+	{
+		NativeObject->CallRemoteFunction(NativeFunction, Params, nullptr, nullptr);
+		return;
+	}
+
+	if (FunctionCallspace & FunctionCallspace::Absorbed)
+	{
+		return;
+	}
+
+	EvaluateInvokePath(NativeObject, NativeFunction, Params, ReturnValueAddress);
 }
 
 void UUObjectExporter::InvokeNativeFunctionOutParms(UObject* NativeObject, UFunction* NativeFunction, uint8* Params, uint8* ReturnValueAddress)
 {
 	TRACE_CPUPROFILER_EVENT_SCOPE(UUObjectExporter::InvokeNativeFunctionOutParms);
 
-	NativeObject->ProcessEvent(NativeFunction, Params);
+    //choose a most fast path. the assumption is that functions defined in U# which are not overridden in BP code can choose a fast path.
+    //if the function calls into BP code we need dedicated stack memory to not risk a stack corruption for BP to use.
+	if (!NativeFunction->HasAnyFunctionFlags(FUNC_Native) && NativeFunction->HasAnyFunctionFlags(FUNC_Event))
+	{
+		NativeObject->ProcessEvent(NativeFunction, Params);
+	}
+	else
+	{
+		FFrame NewStack(NativeObject, NativeFunction, Params, nullptr, NativeFunction->ChildProperties);
+		FOutParmRec** LastOut = &NewStack.OutParms;
+
+		for (TFieldIterator<FProperty> PropIt(NativeFunction); PropIt; ++PropIt)
+		{
+			FProperty* Property = *PropIt;
+
+			if (!Property->HasAllPropertyFlags(CPF_OutParm))
+			{
+				continue;
+			}
+
+			FOutParmRec* Out = static_cast<FOutParmRec*>(UE_VSTACK_ALLOC(VirtualStackAllocator, sizeof(FOutParmRec)));
+
+			Out->PropAddr = Property->ContainerPtrToValuePtr<uint8>(Params);
+			Out->Property = Property;
+
+			if (*LastOut)
+			{
+				(*LastOut)->NextOutParm = Out;
+				LastOut = &(*LastOut)->NextOutParm;
+			}
+			else
+			{
+				*LastOut = Out;
+			}
+		}
+
+		if (*LastOut)
+		{
+			(*LastOut)->NextOutParm = nullptr;
+		}
+
+		NativeFunction->Invoke(NativeObject, NewStack, ReturnValueAddress);
+	}
 }
 
 bool UUObjectExporter::NativeIsValid(UObject* Object)
