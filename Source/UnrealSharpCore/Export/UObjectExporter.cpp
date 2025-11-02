39f3fc282c225582ac2a072e1c799e26a10a527c
Robert Acksel <robert.acksel@googlemail.com>
2025-11-02 11:43:17 +0100

    fix 577 stack frame corruption caused by passing stack memory to BP stack frame which is used for the VW stack then. This leads to writing over the managed stack frame and will damage any memory on the stack leading to program execution corruption.
     keep in mind that return value is handled inside ProcessEvent. A return value address is always +8 bytes behind the params address and the function respects this. This is epics BP memory layout design and can be seen in the function.

--- Source/UnrealSharpCore/Export/UObjectExporter.cpp
+++ Source/UnrealSharpCore/Export/UObjectExporter.cpp
@@ -35,91 +35,29 @@ void UUObjectExporter::InvokeNativeFunction(UObject* NativeObject, UFunction* Na
 {
 	TRACE_CPUPROFILER_EVENT_SCOPE(UUObjectExporter::InvokeNativeFunction);
 
-	if (NativeFunction->HasAllFunctionFlags(FUNC_HasOutParms))
-	{
-		InvokeNativeFunctionOutParms(NativeObject, NativeFunction, Params, ReturnValueAddress);
-	}
-	else
-	{
-		FFrame NewStack(NativeObject, NativeFunction, Params, nullptr, NativeFunction->ChildProperties);
-		NativeFunction->Invoke(NativeObject, NewStack, ReturnValueAddress);
-	}
+	NativeObject->ProcessEvent(NativeFunction, Params);
 }
 
 void UUObjectExporter::InvokeNativeStaticFunction(UClass* NativeClass, UFunction* NativeFunction, uint8* Params, uint8* ReturnValueAddress)
 {
 	TRACE_CPUPROFILER_EVENT_SCOPE(UUObjectExporter::InvokeNativeStaticFunction);
 	UObject* ClassDefaultObject = NativeClass->GetDefaultObject();
-	if (NativeFunction->HasAllFunctionFlags(FUNC_HasOutParms))
-	{
-		InvokeNativeFunctionOutParms(ClassDefaultObject, NativeFunction, Params, ReturnValueAddress);
-	}
-	else
-	{
-		FFrame NewStack(ClassDefaultObject, NativeFunction, Params, nullptr, NativeFunction->ChildProperties);
-		NativeFunction->GetNativeFunc()(ClassDefaultObject, NewStack, ReturnValueAddress);
-	}
+
+	ClassDefaultObject->ProcessEvent(NativeFunction, Params);
 }
 
 void UUObjectExporter::InvokeNativeNetFunction(UObject* NativeObject, UFunction* NativeFunction, uint8* Params, uint8* ReturnValueAddress)
 {
 	TRACE_CPUPROFILER_EVENT_SCOPE(UUObjectExporter::InvokeNativeNetFunction);
 	
-	int32 FunctionCallspace = NativeObject->GetFunctionCallspace(NativeFunction, nullptr);
-		
-	if (FunctionCallspace & FunctionCallspace::Remote)
-	{
-		NativeObject->CallRemoteFunction(NativeFunction, Params, nullptr, nullptr);
-		return;
-	}
-		
-	if (FunctionCallspace & FunctionCallspace::Absorbed)
-	{
-		return;
-	}
-
-	FFrame NewStack(NativeObject, NativeFunction, Params, nullptr, NativeFunction->ChildProperties);
-	NativeFunction->Invoke(NativeObject, NewStack, ReturnValueAddress);
+	NativeObject->ProcessEvent(NativeFunction, Params);
 }
 
 void UUObjectExporter::InvokeNativeFunctionOutParms(UObject* NativeObject, UFunction* NativeFunction, uint8* Params, uint8* ReturnValueAddress)
 {
 	TRACE_CPUPROFILER_EVENT_SCOPE(UUObjectExporter::InvokeNativeFunctionOutParms);
-	
-	FFrame NewStack(NativeObject, NativeFunction, Params, nullptr, NativeFunction->ChildProperties);
-	FOutParmRec** LastOut = &NewStack.OutParms;
-	
-	for (TFieldIterator<FProperty> PropIt(NativeFunction); PropIt; ++PropIt)
-	{
-		FProperty* Property = *PropIt;
-			
-		if (!Property->HasAllPropertyFlags(CPF_OutParm))
-		{
-			continue;
-		}
 
-		FOutParmRec* Out = static_cast<FOutParmRec*>(UE_VSTACK_ALLOC(VirtualStackAllocator, sizeof(FOutParmRec)));
-				
-		Out->PropAddr = Property->ContainerPtrToValuePtr<uint8>(Params);
-		Out->Property = Property;
-				
-		if (*LastOut)
-		{
-			(*LastOut)->NextOutParm = Out;
-			LastOut = &(*LastOut)->NextOutParm;
-		}
-		else
-		{
-			*LastOut = Out;
-		}
-	}
-		
-	if (*LastOut)
-	{
-		(*LastOut)->NextOutParm = nullptr;
-	}
-	
-	NativeFunction->Invoke(NativeObject, NewStack, ReturnValueAddress);
+	NativeObject->ProcessEvent(NativeFunction, Params);
 }
 
 bool UUObjectExporter::NativeIsValid(UObject* Object)
