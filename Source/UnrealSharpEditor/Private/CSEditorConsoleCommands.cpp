bc058859dd92f0d59cf333d64d258b3b3b113713
OlssonDev <olsson-oscar@hotmail.com>
2025-12-22 22:05:06 +0100

    Moved consolecommand.cpp to editor module

--- Source/UnrealSharpEditor/Private/CSEditorConsoleCommands.cpp
+++ Source/UnrealSharpEditor/Private/CSEditorConsoleCommands.cpp
@@ -1,8 +1,7 @@
-#if !UE_BUILD_SHIPPING
-
 #include "CSFieldName.h"
 #include "CSManagedAssembly.h"
 #include "CSManager.h"
+#include "UnrealSharpEditor.h"
 #include "Utilities/CSUtilities.h"
 #include "UnrealSharpUtils.h"
 
@@ -15,20 +14,20 @@ void MetaDataFromMap(const TMap<FName, FString>* PropertyMetaData, int32 IndentL
 {
 	if (!PropertyMetaData || PropertyMetaData->Num() == 0)
 	{
-		UE_LOGFMT(LogUnrealSharp, Log, "{0}(No Metadata Assigned)", Indent(IndentLevel + 1));
+		UE_LOGFMT(LogUnrealSharpEditor, Log, "{0}(No Metadata Assigned)", Indent(IndentLevel + 1));
 		return;
 	}
 	
 	for (const TPair<FName, FString>& MetaDataPair : *PropertyMetaData)
 	{
-		UE_LOGFMT(LogUnrealSharp, Log, "{0}- {1} = {2}", Indent(IndentLevel + 1), *MetaDataPair.Key.ToString(), *MetaDataPair.Value);
+		UE_LOGFMT(LogUnrealSharpEditor, Log, "{0}- {1} = {2}", Indent(IndentLevel + 1), *MetaDataPair.Key.ToString(), *MetaDataPair.Value);
 	}
 }
 
 void DumpMetaData(UField* Field, int32 IndentLevel)
 {
 #if WITH_EDITOR
-	UE_LOGFMT(LogUnrealSharp, Log, "{0}Metadata:", Indent(IndentLevel));
+	UE_LOGFMT(LogUnrealSharpEditor, Log, "{0}Metadata:", Indent(IndentLevel));
 	
 	UPackage* Package = Field->GetOutermost();
 
@@ -48,7 +47,7 @@ void DumpMetaData(UField* Field, int32 IndentLevel)
 void DumpMetaData(FProperty* Property, int32 IndentLevel)
 {
 #if WITH_EDITOR
-	UE_LOGFMT(LogUnrealSharp, Log, "{0}Metadata:", Indent(IndentLevel));
+	UE_LOGFMT(LogUnrealSharpEditor, Log, "{0}Metadata:", Indent(IndentLevel));
 	
 	const TMap<FName, FString>* PropertyMetaData = Property->GetMetaDataMap();
 	MetaDataFromMap(PropertyMetaData, IndentLevel);
@@ -57,7 +56,7 @@ void DumpMetaData(FProperty* Property, int32 IndentLevel)
 
 void DumpPropertiesOfStruct(UStruct* Struct, int32 IndentLevel)
 {
-	UE_LOGFMT(LogUnrealSharp, Log, "{0}Properties:", Indent(IndentLevel));
+	UE_LOGFMT(LogUnrealSharpEditor, Log, "{0}Properties:", Indent(IndentLevel));
 	
 	bool bHasProperties = false;
 	for (TFieldIterator<FProperty> It(Struct, EFieldIterationFlags::None); It; ++It)
@@ -69,13 +68,13 @@ void DumpPropertiesOfStruct(UStruct* Struct, int32 IndentLevel)
 		TArray<const TCHAR*> FlagStrings;
 		FCSUtilities::ParsePropertyFlags(Property->PropertyFlags, FlagStrings);
 		
-		UE_LOGFMT(LogUnrealSharp, Log, "{0}- {1} ({2}) ({3})", Indent(IndentLevel + 1), Property->GetName(), Property->GetClass()->GetName(), FString::Join(FlagStrings, TEXT(", ")));
+		UE_LOGFMT(LogUnrealSharpEditor, Log, "{0}- {1} ({2}) ({3})", Indent(IndentLevel + 1), Property->GetName(), Property->GetClass()->GetName(), FString::Join(FlagStrings, TEXT(", ")));
 		DumpMetaData(Property, IndentLevel + 2);
 	}
 	
 	if (!bHasProperties)
 	{
-		UE_LOGFMT(LogUnrealSharp, Log, "{0}(No Properties Assigned)", Indent(IndentLevel + 1));
+		UE_LOGFMT(LogUnrealSharpEditor, Log, "{0}(No Properties Assigned)", Indent(IndentLevel + 1));
 	}
 }
 
@@ -86,22 +85,22 @@ void DumpDataAsStruct(UStruct* Struct, int32 IndentLevel = 1)
 
 void DumpDataAsTickFunction(const FTickFunction& TickFunction, int32 IndentLevel = 1)
 {
-	UE_LOGFMT(LogUnrealSharp, Log, "{0}Tick Function:", Indent(IndentLevel + 1));
-	UE_LOGFMT(LogUnrealSharp, Log, "{0}- bCanEverTick: {1}", Indent(IndentLevel + 2), TickFunction.bCanEverTick ? TEXT("true") : TEXT("false"));
-	UE_LOGFMT(LogUnrealSharp, Log, "{0}- bStartWithTickEnabled: {1}", Indent(IndentLevel + 2), TickFunction.bStartWithTickEnabled ? TEXT("true") : TEXT("false"));
-	UE_LOGFMT(LogUnrealSharp, Log, "{0}- TickInterval: {1}", Indent(IndentLevel + 2), TickFunction.TickInterval);
-	UE_LOGFMT(LogUnrealSharp, Log, "{0}- TickGroup: {1}", Indent(IndentLevel + 2), static_cast<int32>(TickFunction.TickGroup));
+	UE_LOGFMT(LogUnrealSharpEditor, Log, "{0}Tick Function:", Indent(IndentLevel + 1));
+	UE_LOGFMT(LogUnrealSharpEditor, Log, "{0}- bCanEverTick: {1}", Indent(IndentLevel + 2), TickFunction.bCanEverTick ? TEXT("true") : TEXT("false"));
+	UE_LOGFMT(LogUnrealSharpEditor, Log, "{0}- bStartWithTickEnabled: {1}", Indent(IndentLevel + 2), TickFunction.bStartWithTickEnabled ? TEXT("true") : TEXT("false"));
+	UE_LOGFMT(LogUnrealSharpEditor, Log, "{0}- TickInterval: {1}", Indent(IndentLevel + 2), TickFunction.TickInterval);
+	UE_LOGFMT(LogUnrealSharpEditor, Log, "{0}- TickGroup: {1}", Indent(IndentLevel + 2), static_cast<int32>(TickFunction.TickGroup));
 }
 
 void DumpDataAsClass(UClass* Class, int32 IndentLevel = 1)
 {
 	TArray<const TCHAR*> ClassFlagsStrings;
 	FCSUtilities::ParseClassFlags(Class->ClassFlags, ClassFlagsStrings);
-	UE_LOGFMT(LogUnrealSharp, Log, "{0}Class Flags: ({1})", Indent(IndentLevel + 1), FString::Join(ClassFlagsStrings, TEXT(", ")));
+	UE_LOGFMT(LogUnrealSharpEditor, Log, "{0}Class Flags: ({1})", Indent(IndentLevel + 1), FString::Join(ClassFlagsStrings, TEXT(", ")));
 	
 	DumpPropertiesOfStruct(Class, IndentLevel + 1);
 
-	UE_LOGFMT(LogUnrealSharp, Log, "{0}Functions:", Indent(IndentLevel + 1));
+	UE_LOGFMT(LogUnrealSharpEditor, Log, "{0}Functions:", Indent(IndentLevel + 1));
 	
 	bool bHasFunctions = false;
 	for (TFieldIterator<UFunction> It(Class, EFieldIterationFlags::None); It; ++It)
@@ -112,7 +111,7 @@ void DumpDataAsClass(UClass* Class, int32 IndentLevel = 1)
 		TArray<const TCHAR*> FunctionFlagsStrings;
 		FCSUtilities::ParseFunctionFlags(Function->FunctionFlags, FunctionFlagsStrings);
 
-		UE_LOGFMT(LogUnrealSharp, Log, "{0}- {1} ({2})", Indent(IndentLevel + 2), Function->GetName(), FString::Join(FunctionFlagsStrings, TEXT(", ")));
+		UE_LOGFMT(LogUnrealSharpEditor, Log, "{0}- {1} ({2})", Indent(IndentLevel + 2), Function->GetName(), FString::Join(FunctionFlagsStrings, TEXT(", ")));
 		DumpMetaData(Function, IndentLevel + 3);
 		DumpPropertiesOfStruct(Function, IndentLevel + 3);
 	}
@@ -131,17 +130,17 @@ void DumpDataAsClass(UClass* Class, int32 IndentLevel = 1)
 	
 	if (!bHasFunctions)
 	{
-		UE_LOGFMT(LogUnrealSharp, Log, "{0}(No Functions Assigned)", Indent(IndentLevel + 2));
+		UE_LOGFMT(LogUnrealSharpEditor, Log, "{0}(No Functions Assigned)", Indent(IndentLevel + 2));
 	}
 }
 
 void DumpDataAsEnum(UEnum* Enum, int32 IndentLevel = 1)
 {
-	UE_LOGFMT(LogUnrealSharp, Log, "{0}Enum Values:", Indent(IndentLevel + 1));
+	UE_LOGFMT(LogUnrealSharpEditor, Log, "{0}Enum Values:", Indent(IndentLevel + 1));
 
 	for (int32 i = 0; i < Enum->NumEnums(); i++)
 	{
-		UE_LOGFMT(LogUnrealSharp, Log, "{0}- {1} = {2}", Indent(IndentLevel + 2), Enum->GetNameStringByIndex(i), Enum->GetValueByIndex(i));
+		UE_LOGFMT(LogUnrealSharpEditor, Log, "{0}- {1} = {2}", Indent(IndentLevel + 2), Enum->GetNameStringByIndex(i), Enum->GetValueByIndex(i));
 	}
 }
 
@@ -154,7 +153,7 @@ void DumpTypeReflectionData(const TArray<FString>& Args)
 {
 	if (Args.Num() != 1)
 	{
-		UE_LOG(LogUnrealSharp, Warning, TEXT("Usage: UnrealSharp.DumpTypeReflectionData [TypeFullName]"));
+		UE_LOG(LogUnrealSharpEditor, Warning, TEXT("Usage: UnrealSharp.DumpTypeReflectionData [TypeFullName]"));
 		return;
 	}
 
@@ -162,7 +161,7 @@ void DumpTypeReflectionData(const TArray<FString>& Args)
 	
 	if (!TypeFullName.Contains("."))
 	{
-		UE_LOG(LogUnrealSharp, Warning, TEXT("TypeFullName must be in the format 'Namespace.TypeName'"));
+		UE_LOG(LogUnrealSharpEditor, Warning, TEXT("TypeFullName must be in the format 'Namespace.TypeName'"));
 		return;
 	}
 	
@@ -190,11 +189,11 @@ void DumpTypeReflectionData(const TArray<FString>& Args)
 		
 		if (!IsValid(Field))
 		{
-			UE_LOGFMT(LogUnrealSharp, Warning, "Managed type found but no associated UField: {0}", *TypeFullName);
+			UE_LOGFMT(LogUnrealSharpEditor, Warning, "Managed type found but no associated UField: {0}", *TypeFullName);
 			return;
 		}
 		
-		UE_LOGFMT(LogUnrealSharp, Log, "Reflection data for type: {0}", *TypeFullName);
+		UE_LOGFMT(LogUnrealSharpEditor, Log, "Reflection data for type: {0}", *TypeFullName);
 		
 		DumpMetaData(Field, 2);
 		
@@ -216,13 +215,13 @@ void DumpTypeReflectionData(const TArray<FString>& Args)
 		}
 		else
 		{
-			UE_LOG(LogUnrealSharp, Warning, TEXT("Unsupported type: %s"), *Field->GetClass()->GetName());
+			UE_LOG(LogUnrealSharpEditor, Warning, TEXT("Unsupported type: %s"), *Field->GetClass()->GetName());
 		}
 		
 		return;
 	}
 	
-	UE_LOGFMT(LogUnrealSharp, Warning, "Type not found in any Assembly: {0}", *TypeFullName);
+	UE_LOGFMT(LogUnrealSharpEditor, Warning, "Type not found in any Assembly: {0}", *TypeFullName);
 }
 
 static FAutoConsoleCommand CVarDumpTypeReflectionData(
@@ -239,10 +238,10 @@ static FAutoConsoleCommand CVarDumpLoadedAssemblies(
 		TArray<UCSManagedAssembly*> Assemblies;
 		UCSManager::Get().GetLoadedAssemblies(Assemblies);
 		
-		UE_LOG(LogUnrealSharp, Log, TEXT("Loaded Managed Assemblies:"));
+		UE_LOG(LogUnrealSharpEditor, Log, TEXT("Loaded Managed Assemblies:"));
 		for (UCSManagedAssembly* Assembly : Assemblies)
 		{
-			UE_LOGFMT(LogUnrealSharp, Log, "- {0}", *Assembly->GetAssemblyName().ToString());
+			UE_LOGFMT(LogUnrealSharpEditor, Log, "- {0}", *Assembly->GetAssemblyName().ToString());
 		}
 	})
 );
@@ -254,7 +253,7 @@ static FAutoConsoleCommand CVarListTypesInAssembly(
 	{
 		if (Args.Num() != 1)
 		{
-			UE_LOG(LogUnrealSharp, Warning, TEXT("Usage: UnrealSharp.ListTypesInAssembly [AssemblyName]"));
+			UE_LOG(LogUnrealSharpEditor, Warning, TEXT("Usage: UnrealSharp.ListTypesInAssembly [AssemblyName]"));
 			return;
 		}
 		
@@ -263,11 +262,11 @@ static FAutoConsoleCommand CVarListTypesInAssembly(
 		
 		if (!IsValid(Assembly))
 		{
-			UE_LOGFMT(LogUnrealSharp, Warning, "Assembly not found: {0}", *AssemblyName.ToString());
+			UE_LOGFMT(LogUnrealSharpEditor, Warning, "Assembly not found: {0}", *AssemblyName.ToString());
 			return;
 		}
 		
-		UE_LOGFMT(LogUnrealSharp, Log, "Managed Types in Assembly {0}:", *AssemblyName.ToString());
+		UE_LOGFMT(LogUnrealSharpEditor, Log, "Managed Types in Assembly {0}:", *AssemblyName.ToString());
 		
 		for (const TTuple<FCSFieldName, TSharedPtr<FCSManagedTypeDefinition>>& Pair : Assembly->GetDefinedManagedTypes())
 		{
@@ -279,20 +278,18 @@ static FAutoConsoleCommand CVarListTypesInAssembly(
 				UField* Field = TypeDefinition->GetDefinitionField();
 				if (IsValid(Field))
 				{
-					UE_LOGFMT(LogUnrealSharp, Log, "- {0} ({1})", *FieldName.GetFullName().ToString(), Field->GetClass()->GetName());
+					UE_LOGFMT(LogUnrealSharpEditor, Log, "- {0} ({1})", *FieldName.GetFullName().ToString(), Field->GetClass()->GetName());
 				}
 				else
 				{
-					UE_LOGFMT(LogUnrealSharp, Log, "- {0} (No UField)", *FieldName.GetFullName().ToString());
+					UE_LOGFMT(LogUnrealSharpEditor, Log, "- {0} (No UField)", *FieldName.GetFullName().ToString());
 				}
 			}
 			else
 			{
-				UE_LOGFMT(LogUnrealSharp, Log, "- {0} (No Type Definition)", *FieldName.GetFullName().ToString());
+				UE_LOGFMT(LogUnrealSharpEditor, Log, "- {0} (No Type Definition)", *FieldName.GetFullName().ToString());
 			}
 		}
 	})
 );
-
-#endif // !UE_BUILD_SHIPPING
 
