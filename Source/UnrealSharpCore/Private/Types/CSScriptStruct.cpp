1d9e4591683f7733a314444b6dfdffc04e210f93
Oscar Olsson <101010793+OlssonDev@users.noreply.github.com>
2026-01-16 19:21:10 +0100

    Fixed struct defaults copy corrupting memory

--- Source/UnrealSharpCore/Private/Types/CSScriptStruct.cpp
+++ Source/UnrealSharpCore/Private/Types/CSScriptStruct.cpp
@@ -8,6 +8,7 @@ void UCSScriptStruct::Initialize()
 	
 	InitializeStructDefaults();
 	DefaultStructInstance = FUserStructOnScopeIgnoreDefaults(this, StructDefaults.Get());
+	DefaultStructInstance.SetPackage(GetOutermost());
 	UpdateStructFlags();
 }
 
@@ -15,39 +16,26 @@ void UCSScriptStruct::InitializeStruct(void* Dest, int32 ArrayDim) const
 {
 	TRACE_CPUPROFILER_EVENT_SCOPE(UCSScriptStruct::InitializeStruct);
 	
-	int32 Size = GetStructureSize();
-	uint8* DefaultData = StructDefaults.Get();
-	
-	if (Size <= 0 || !DefaultData)
+	if ((StructFlags & STRUCT_IsPlainOldData) == 0)
 	{
-		return;
+		InitializeStructIgnoreDefaults(Dest, ArrayDim);
 	}
 	
-	for (FProperty* Property = PropertyLink; Property; Property = Property->PropertyLinkNext)
+	int32 Size = GetStructureSize();
+	
+	for (int32 ArrayIndex = 0; ArrayIndex < ArrayDim; ArrayIndex++)
 	{
-		auto CalculateOffset = [](FProperty* Property, int32 Index, int32 Size, void* Data) -> uint8*
-		{
-			return static_cast<uint8*>(Data) + Index * Size + Property->GetOffset_ForInternal();
-		};
-		
-		for (int32 Index = 0; Index < ArrayDim; Index++)
-		{
-			uint8* DestPtr = CalculateOffset(Property, Index, Size, Dest);
-			uint8* SrcPtr = CalculateOffset(Property, Index, Size, DefaultData);
-			Property->CopyCompleteValue(DestPtr, SrcPtr);
-		}
+		void* DestStruct = static_cast<uint8*>(Dest) + (Size * ArrayIndex);
+		CopyScriptStruct(DestStruct, StructDefaults.Get());
 	}
 }
 
 void UCSScriptStruct::InitializeStructDefaults()
 {
-	int32 Size = GetStructureSize();
-	
-	if (Size <= 0)
-	{
-		return;
-	}
-	
+	int32 Size = FMath::Max(GetStructureSize(), 1);
 	StructDefaults = MakeUnique<uint8[]>(Size);
+	
+	InitializeStructIgnoreDefaults(StructDefaults.Get());
+	
 	FCSManagedCallbacks::ManagedCallbacks.InitializeStructure(GetTypeGCHandle()->GetHandle(), StructDefaults.Get());
 }
