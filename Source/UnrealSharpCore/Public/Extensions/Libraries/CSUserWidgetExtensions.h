ce531bf2235a4176be57fb32e9529a146001a689
DAHAI LI <blizzzrdda@gmail.com>
2026-02-06 12:50:15 -0500

    Add CreateWidget overload using owning widget

--- Source/UnrealSharpCore/Public/Extensions/Libraries/CSUserWidgetExtensions.h
+++ Source/UnrealSharpCore/Public/Extensions/Libraries/CSUserWidgetExtensions.h
@@ -6,6 +6,21 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "CSUserWidgetExtensions.generated.h"
 
+/**
+ * UCSUserWidgetExtensions
+ *
+ * Purpose:
+ *   - Exposes UUserWidget helpers to managed code via UnrealSharp script methods.
+ *
+ * Responsibilities:
+ *   - Provide owning player/state accessors for user widgets.
+ *   - Create user widgets from world context objects or parent widgets.
+ *   - Enumerate widget tree children for managed callers.
+ *
+ * Dependencies:
+ *   - UWidgetBlueprintLibrary for widget creation.
+ *   - UWidgetTree for widget hierarchy traversal.
+ */
 UCLASS(meta = (InternalType))
 class UCSUserWidgetExtensions : public UBlueprintFunctionLibrary
 {
@@ -25,6 +40,9 @@ public:
 
 	UFUNCTION(meta=(ScriptMethod, UserWidgetClass = "/Script/UMG.UserWidget", DeterminesOutputType = "UserWidgetClass"))
 	static UUserWidget* CreateWidget(UObject* WorldContextObject, const TSubclassOf<UUserWidget>& UserWidgetClass, APlayerController* OwningController);
+
+	UFUNCTION(meta=(ScriptMethod, UserWidgetClass = "/Script/UMG.UserWidget", DeterminesOutputType = "UserWidgetClass"))
+	static UUserWidget* CreateWidget_WithWidget(UUserWidget* OwningWidget, const TSubclassOf<UUserWidget>& UserWidgetClass);
 
 	UFUNCTION(meta=(ScriptMethod))
 	static TArray<UWidget*> GetAllWidgets(UUserWidget* UserWidget);
