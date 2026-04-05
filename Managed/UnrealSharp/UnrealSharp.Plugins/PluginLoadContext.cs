d58098a65bc42b2e8de2ebd9e72a36573546add6
Klip29mat2 <klipmat@proton.me>
2026-04-05 10:59:28 +0200

    Fix PluginLoadContext Load method

--- Managed/UnrealSharp/UnrealSharp.Plugins/PluginLoadContext.cs
+++ Managed/UnrealSharp/UnrealSharp.Plugins/PluginLoadContext.cs
@@ -1,51 +1,63 @@
 using System.Reflection;
 using System.Runtime.Loader;
+using System.IO;
 
 namespace UnrealSharp.Plugins;
 
 public class PluginLoadContext : AssemblyLoadContext
 {
-	private readonly AssemblyDependencyResolver _resolver;
+    private readonly AssemblyDependencyResolver _resolver;
 
-	public PluginLoadContext(string pluginName, AssemblyDependencyResolver resolver, bool isCollectible) : base(pluginName, isCollectible)
-	{
-		_resolver = resolver;
-	}
+    public PluginLoadContext(string pluginName, AssemblyDependencyResolver resolver, bool isCollectible) 
+        : base(pluginName, isCollectible)
+    {
+        _resolver = resolver;
+    }
 
-	protected override Assembly? Load(AssemblyName assemblyName)
-	{
-		if (string.IsNullOrEmpty(assemblyName.Name))
-		{
-			return null;
-		}
+    protected override Assembly? Load(AssemblyName assemblyName)
+    {
+        if (string.IsNullOrEmpty(assemblyName.Name))
+        {
+            return null;
+        }
 
-		Assembly? loadedAssembly = AssemblyCache.GetAssembly(assemblyName.Name!, this);
-		if (loadedAssembly != null)
-		{
-			return loadedAssembly;
-		}
+        // cache
+        Assembly? loadedAssembly = AssemblyCache.GetAssembly(assemblyName.Name!, this);
+        if (loadedAssembly != null)
+        {
+            return loadedAssembly;
+        }
 
-		string? assemblyPath = _resolver.ResolveAssemblyToPath(assemblyName);
-		if (string.IsNullOrEmpty(assemblyPath))
-		{
-			return null;
-		}
+        // resolver local
+        string? assemblyPath = _resolver.ResolveAssemblyToPath(assemblyName);
+        if (!string.IsNullOrEmpty(assemblyPath))
+        {
+            using FileStream assemblyFile = File.Open(assemblyPath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            string pdbPath = Path.ChangeExtension(assemblyPath, ".pdb");
 
-		using FileStream assemblyFile = File.Open(assemblyPath, FileMode.Open, FileAccess.Read, FileShare.Read);
-		string pdbPath = Path.ChangeExtension(assemblyPath, ".pdb");
+            Assembly newAssembly;
+            if (!File.Exists(pdbPath))
+            {
+                newAssembly = LoadFromAssemblyPath(assemblyPath);
+            }
+            else
+            {
+                using FileStream pdbFile = File.Open(pdbPath, FileMode.Open, FileAccess.Read, FileShare.Read);
+                newAssembly = LoadFromStream(assemblyFile, pdbFile);
+            }
 
-		Assembly? newAssembly;
-		if (!File.Exists(pdbPath))
-		{
-			newAssembly = LoadFromAssemblyPath(assemblyPath);
-		}
-		else
-		{
-			using FileStream pdbFile = File.Open(pdbPath, FileMode.Open, FileAccess.Read, FileShare.Read);
-			newAssembly = LoadFromStream(assemblyFile, pdbFile);
-		}
-        
-		AssemblyCache.AddAssembly(newAssembly);
-		return newAssembly;
-	}
+            AssemblyCache.AddAssembly(newAssembly);
+            return newAssembly;
+        }
+
+        // FALLBACK
+        try
+        {
+            return AssemblyLoadContext.Default.LoadFromAssemblyName(assemblyName);
+        }
+        catch
+        {
+            return null;
+        }
+    }
 }
