d5c58572b62075f88fdeb8151b817cba982417c4
Oscar Olsson <101010793+OlssonDev@users.noreply.github.com>
2026-05-16 02:07:23 +0200

    Fixed gitignore, ignoring files in automation project

---
+++ Build/Scripts/BuildCommands/BuildUserSolution.cs
@@ -0,0 +1,16 @@
+﻿using AutomationTool;
+using UnrealBuildTool;
+using UnrealSharp.Automation.Utilities;
+
+namespace UnrealSharp.Automation.BuildCommands;
+
+[Help("BuildUserSolution", "Builds the UnrealSharp user solution (.sln) for the specified build configuration.")]
+[Help("BuildConfig=<Config>", "The build configuration (Debug, DebugGame, Development, Shipping, etc.).")]
+public class BuildUserSolution : BuildCommand
+{
+    public override void ExecuteBuild()
+    {
+        UnrealTargetConfiguration BuildConfig = ParseOptionalEnumParam<UnrealTargetConfiguration>("BuildConfig") ?? UnrealTargetConfiguration.Development;
+        BuildCommands.BuildSolution.RunBuild(this.GetProjectScriptFolder(), BuildConfig, false);
+    }
+}
