8c11e8c255b62d05825e6304c5042fd48a58d1bf
Oscar Olsson <101010793+OlssonDev@users.noreply.github.com>
2026-06-06 13:29:33 +0200

    Changed cast to UCSDelegateFunction in UCSManagedDelegateCompiler

--- Source/UnrealSharpCore/Private/Compilers/CSManagedDelegateCompiler.cpp
+++ Source/UnrealSharpCore/Private/Compilers/CSManagedDelegateCompiler.cpp
@@ -11,7 +11,7 @@ UCSManagedDelegateCompiler::UCSManagedDelegateCompiler()
 
 void UCSManagedDelegateCompiler::Compile(UField* TypeToRecompile, const TSharedPtr<FCSManagedTypeDefinition>& ManagedTypeDefinition) const
 {
-	UDelegateFunction* DelegateSignature = static_cast<UDelegateFunction*>(TypeToRecompile);
+	UCSDelegateFunction* DelegateSignature = static_cast<UCSDelegateFunction*>(TypeToRecompile);
 	TSharedPtr<FCSFunctionReflectionData> FunctionReflectionData = ManagedTypeDefinition->GetReflectionData<FCSFunctionReflectionData>();
 	
 	FCSUnrealSharpUtils::PurgeStruct(DelegateSignature);
