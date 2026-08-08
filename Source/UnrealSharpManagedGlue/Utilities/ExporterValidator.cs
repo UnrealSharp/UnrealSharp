97c32b0fe785c4869164fc1b8ddd6609d30f92a2
Oscar Olsson <101010793+OlssonDev@users.noreply.github.com>
2026-08-08 17:36:04 +0200

    Assembly compilation stamp removed in 5.8. Created our own to make the c++ > c# exporter stale when new code is introduced

--- Source/UnrealSharpManagedGlue/Utilities/ExporterValidator.cs
+++ Source/UnrealSharpManagedGlue/Utilities/ExporterValidator.cs
@@ -1,58 +1,77 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Reflection;
-using UnrealSharp.Shared;
 
 namespace UnrealSharpManagedGlue.Utilities;
 
 public static class ExporterValidator
 {
-    static bool IsExporterStale()
-    {
-        string executingAssemblyPath = Assembly.GetExecutingAssembly().Location;
-        DateTime executingAssemblyLastWriteTime = File.GetLastWriteTimeUtc(executingAssemblyPath);
+	private static string AssemblyModificationTimeFile => Path.Combine(GeneratorStatics.PluginModule.OutputDirectory, "GlueGeneratorTimestamp");
 
-        string generatedCodeDirectory = GeneratorStatics.PluginModule.OutputDirectory;
-        string timestampFilePath = Path.Combine(generatedCodeDirectory, "Timestamp");
+	static bool HasAssemblyRecentlyChanged()
+	{
+		string executingAssemblyPath = Assembly.GetExecutingAssembly().Location;
+		DateTime executingAssemblyLastWriteTime = File.GetLastWriteTimeUtc(executingAssemblyPath);
+		
+		if (!File.Exists(AssemblyModificationTimeFile))
+		{
+			return true;
+		}
 
-        if (!File.Exists(timestampFilePath) || !Directory.Exists(GeneratorStatics.PluginDirectory))
-        {
-            return true;
-        }
+		string savedTimestamp = File.ReadAllText(AssemblyModificationTimeFile);
+		if (!DateTime.TryParse(savedTimestamp, CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind, out DateTime savedTimestampUtc))
+		{
+			return true;
+		}
 
-        DateTime savedTimestampUtc = File.GetLastWriteTimeUtc(timestampFilePath);
-        return executingAssemblyLastWriteTime > savedTimestampUtc;
-    }
+		return executingAssemblyLastWriteTime > savedTimestampUtc;
+	}
 
-    public static void ValidateExporter()
-    {
-        ConsoleUtilities.Log("Validating exporter...");
+	public static void ValidateExporter()
+	{
+		ConsoleUtilities.Log("Validating exporter...");
 
-        if (!IsExporterStale())
-        {
-            PackageHeadersTracker.DeserializeModuleHeaders();
-            return;
-        }
+		if (!HasAssemblyRecentlyChanged())
+		{
+			PackageHeadersTracker.DeserializeModuleHeaders();
+			return;
+		}
 
-        ConsoleUtilities.Log("Exporter is stale. Re-exporting all bindings...");
-        CleanModules();
-    }
+		ConsoleUtilities.Log("Exporter is stale. Re-exporting all bindings...");
+		CleanModules();
+		CreateTimestamp();
+	}
 
-    private static void CleanModules()
-    {
-        IEnumerable<ModuleInfo> modules = ModuleUtilities.PackageToModuleInfo.Values;
-        
-        foreach (ModuleInfo module in modules)
-        {
-            FileExporter.CleanGeneratedFolder(module.Module.GetPackageOutputDirectory());
+	private static void CreateTimestamp()
+	{
+		string executingAssemblyPath = Assembly.GetExecutingAssembly().Location;
+		DateTime executingAssemblyLastWriteTime = File.GetLastWriteTimeUtc(executingAssemblyPath);
 
-            if (module.IsPartOfEngine || !File.Exists(module.CsProjPath))
-            {
-                continue;
-            }
+		string? directory = Path.GetDirectoryName(AssemblyModificationTimeFile);
+		if (!string.IsNullOrEmpty(directory))
+		{
+			Directory.CreateDirectory(directory);
+		}
 
-            File.Delete(module.CsProjPath);
-        }
-    }
+		File.WriteAllText(AssemblyModificationTimeFile, executingAssemblyLastWriteTime.ToString("o"));
+	}
+
+	private static void CleanModules()
+	{
+		IEnumerable<ModuleInfo> modules = ModuleUtilities.PackageToModuleInfo.Values;
+
+		foreach (ModuleInfo module in modules)
+		{
+			FileExporter.CleanGeneratedFolder(module.Module.GetPackageOutputDirectory());
+
+			if (module.IsPartOfEngine || !File.Exists(module.CsProjPath))
+			{
+				continue;
+			}
+
+			File.Delete(module.CsProjPath);
+		}
+	}
 }
