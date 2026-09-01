e262b43569d94274a26f347b46fa2c8b5e8edefa
Steve <hez2010@outlook.com>
2026-09-01 19:50:09 +0900

    Use dotnet executable in selected sdk

--- Build/Scripts/Processes/DotnetProcess.cs
+++ Build/Scripts/Processes/DotnetProcess.cs
@@ -8,6 +8,7 @@ public class DotnetProcess : BuildToolProcess
     public DotnetProcess() : base(DotNetUtilities.DotNetExecutable)
     {
         string LatestDotNetSdkPath = DotNetUtilities.LatestDotNetSdkPath;
+        StartInfo.ArgumentList.Add(Path.Combine(LatestDotNetSdkPath, "dotnet.dll"));
         StartInfo.Environment["MSBuildExtensionsPath"] = LatestDotNetSdkPath;
         StartInfo.Environment["MSBUILD_EXE_PATH"] = Path.Combine(LatestDotNetSdkPath, "MSBuild.dll");
         StartInfo.Environment["MSBuildSDKsPath"] = Path.Combine(LatestDotNetSdkPath, "Sdks");
