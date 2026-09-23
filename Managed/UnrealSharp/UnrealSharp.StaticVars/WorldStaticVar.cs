5a693c7ff9284ddb3863e15a6d7eb8eb40327d18
Can Göktaş <cangoktas@nargamestudio.com>
2026-09-23 13:53:02 +0300

    Fix editor fails to open because of unrealsharp.staticvars

--- Managed/UnrealSharp/UnrealSharp.StaticVars/WorldStaticVar.cs
+++ Managed/UnrealSharp/UnrealSharp.StaticVars/WorldStaticVar.cs
@@ -18,11 +18,13 @@ public sealed class FWorldStaticVar<T> : FBaseStaticVar<T>
     
     [DebuggerBrowsable(DebuggerBrowsableState.Never)]
     private readonly FDelegateHandle _onWorldCleanupHandle;
-    
+
+    private readonly FWorldDelegates.FWorldCleanupEvent _onWorldCleanupDelegate;
+
     public FWorldStaticVar()
     {
-        FWorldDelegates.FWorldCleanupEvent onWorldCleanupDelegate = OnWorldCleanup;
-        IntPtr onWorldCleanup = Marshal.GetFunctionPointerForDelegate(onWorldCleanupDelegate);
+        _onWorldCleanupDelegate = OnWorldCleanup;
+        IntPtr onWorldCleanup = Marshal.GetFunctionPointerForDelegate(_onWorldCleanupDelegate);
         Bind_FWorldDelegates.CallBindOnWorldCleanup(onWorldCleanup, out _onWorldCleanupHandle);
     }
     
